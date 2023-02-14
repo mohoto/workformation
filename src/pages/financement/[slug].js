a1f16278e57a12f3ab0e6a6a9764b40c61b3abfd
Walid Benmouffok <walid.benmouffok@gmail.com>
2023-02-14 17:08:13 +0100

    rectif sidebar financement

--- src/pages/financement/[slug].js
+++ src/pages/financement/[slug].js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import {useRouter} from 'next/router'
 import axios from 'axios'
 import Content from '../../components/financements/Content'
-import Sidebar from '../../components/financements/SideBar'
+import Sidebar from '../../components/financements/Sidebar'
 
 export default function Financemnt() {
 
