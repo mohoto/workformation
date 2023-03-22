8278001cb210ac266c051b6538361afbf863cb56
Walid Benmouffok <walid.benmouffok@gmail.com>
2023-03-22 18:01:51 +0100

    ajout sitemap 2

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.workformation.fr/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
