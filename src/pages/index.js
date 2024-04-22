cef1780183014316fe99634ae0e6005d4e8f03ef
Walid Benmouffok <walid.benmouffok@gmail.com>
2024-04-22 15:54:06 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export async function getStaticProps() {
   const {data: articles} = await axios.get(`${url}/api/articles`);
   return {
     props: {
-      selectedFormations: formations.filter(formation => formation.id === "1" || formation.id === "11" || formation.id === "6"),
+      selectedFormations: formations.filter(formation => formation.id === "1" || formation.id === "11" || formation.id === "9"),
       selectedArticles: articles.filter(article => article.id === "1" || article.id === "6" || article.id === "2"),
     }
   }
